82c87bdef903e1b722be67b141ea9d2ae0f0bc67
nomad <pramuk@live.ru>
2026-09-09 16:27:02 +1000

    init commit

---
+++ src/main/java/com/example/privateclub/PrivateclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.privateclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/privateclub/PrivateclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privateclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
